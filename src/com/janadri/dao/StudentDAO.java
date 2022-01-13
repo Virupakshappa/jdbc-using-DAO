36544a8974438a5b3bc5ba8a4b7aff99b5434923
Veeru Janadri <vjanadri7@gmail.com>
2022-01-13 14:58:09 +0530

    Updating the repo with add student details method

--- src/com/janadri/dao/StudentDAO.java
+++ src/com/janadri/dao/StudentDAO.java
@@ -7,13 +7,16 @@ public class StudentDAO {
     String uri = "jdbc:mysql://localhost/itachi";
     String user = "root";
     String pwd = "";
-
+    Connection con = null;
+    public void connect() throws Exception{
+        //        Class.forName("com.mysql.jdbc.Driver");
+        con = DriverManager.getConnection(uri, user, pwd);
+    }
     public Student getStudent(int rollno) throws Exception {
         String query = "select sname from student where rollno= " + rollno;
         Student s = new Student();
         s.rollno = rollno;
-//        Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection(uri, user, pwd);
+
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(query);
         rs.next();
@@ -21,5 +24,15 @@ public class StudentDAO {
         s.sname = name;
 
         return s;
-    }
+
+        /*for adding the details to student table use the below method*/
+
+//public void addStudent(Student s) throws Exception {
+//    String query = "insert into student values(?,?)";
+//    PreparedStatement pst = con.prepareStatement(query);
+//    pst.setInt(1, s.rollno);
+//    pst.setString(2, s.sname);
+//    pst.executeUpdate();
+//
+   }
 }
--- src/com/janadri/jdbc/JdbcDaoDemo.java
+++ src/com/janadri/jdbc/JdbcDaoDemo.java
@@ -6,7 +6,15 @@ import com.janadri.pojo.*;
 public class JdbcDaoDemo {
     public static void main(String[] args) throws Exception{
         StudentDAO dao = new StudentDAO();
+        dao.connect();
         Student s1 = dao.getStudent(12);
         System.out.println(s1.sname);
+
+        //for adding the student details
+//        Student s2 = new Student();
+//        s2.sname="Ashwath";
+//        s2.rollno=7;
+//        dao.connect();
+//       dao.addStudent(s2);
     }
 }
